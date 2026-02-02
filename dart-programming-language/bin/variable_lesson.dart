744739a58500e9638cc9b078a70dc101ef818fcf
Ruby Wai <62864134+rubywai@users.noreply.github.com>
2026-02-02 23:38:35 +0800

    add data type

---
+++ dart-programming-language/bin/variable_lesson.dart
@@ -0,0 +1,7 @@
+void main(){
+  String name = "Wai Phyo"; //camel case
+  String name2 = "Aung Aung"; //camel case
+  int ageOfPerson = 28;
+
+  print("My name is $name Age is $ageOfPerson");//string interpolation
+}
